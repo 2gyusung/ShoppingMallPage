72ea34683782f0037536fcd59f0b2c710874956a
이규성 <leegs005@naver.com>
2025-02-27 13:05:38 +0900

    리덕스 어렵다ㅇ... 다시 보ㅓ습.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
-import {applyMiddleware, createStore, } from 'redux'
+import {applyMiddleware, } from 'redux'
 import rootReducer from "./reducers/index"
 import { thunk } from 'redux-thunk'
+import { configureStore } from '@reduxjs/toolkit'
+import authenciateReducer from './reducers/authenticateReducer'
+import productReducer from "./productReducer";
 
-let store = createStore(rootReducer,applyMiddleware(thunk))
+let store = configureStore({
+  reducer: {
+    auth : authenciateReducer,
+  product : productReducer
+  }
+})
 
 export default store
